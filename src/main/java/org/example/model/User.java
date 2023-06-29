bb1c2275a4db820cb3095e300908645733bf9c1d
mohaliza138 <mohaliza138@gmail.com>
2023-06-29 13:01:47 +0330

    User implemented.

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,6 +1,7 @@
 package org.example.model;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class User {
     public static User user;
@@ -10,5 +11,18 @@ public class User {
     public User (String name) {
         this.name = name;
         this.maps = new ArrayList<>();
+    }
+    
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name) && maps.equals(user.maps);
+    }
+    
+    @Override
+    public int hashCode () {
+        return Objects.hash(name, maps);
     }
 }
