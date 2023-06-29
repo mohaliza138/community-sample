f65e84b53b326e78fda13b72f366e9e0ba017beb
mohaliza138 <mohaliza138@gmail.com>
2023-06-29 13:02:54 +0330

    GameMap implemented.

--- src/main/java/org/example/model/GameMap.java
+++ src/main/java/org/example/model/GameMap.java
@@ -1,9 +1,26 @@
 package org.example.model;
 
+import java.util.Objects;
+
 public class GameMap {
     public final String name;
+    public final User user;
     
-    public GameMap (String name) {
+    public GameMap (String name, User user) {
         this.name = name;
+        this.user = user;
+    }
+    
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameMap gameMap = (GameMap) o;
+        return name.equals(gameMap.name) && user.equals(gameMap.user);
+    }
+    
+    @Override
+    public int hashCode () {
+        return Objects.hash(name, user);
     }
 }
